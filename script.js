c0732c1c96ce08f9170718982329863acc210728
its-davis <80759908+its-davis@users.noreply.github.com>
2023-03-19 23:38:22 +0530

    add and view franchisee

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
+    do_list_franchisee_table();
+
     $('#formula').on('change', function (e) {
         console.log('selected revenue type: ', this.value);
         $('#selectFranchise').empty();
@@ -6,7 +8,6 @@ $(document).ready(function () {
         $.ajax({
             type: "POST",
             url: 'http://192.168.0.134:5000/ajax_sample/'+this.value,
-            // dataType: 'application/json',
             success: function (response_data) {
                 console.log('response_data: ', response_data);
                 let ops = '<option selected disabled>-- CLick to select --</option>'
@@ -26,6 +27,8 @@ $(document).ready(function () {
         $(`body > div.`+div_class).show()
         console.log(link_class, div_class);
     });
+
+    $(`#add-franchisee-form`).on('submit', handle_form_add_franchisee);
 });
 
 let link_main_class_names = {
@@ -34,4 +37,46 @@ let link_main_class_names = {
     "menu-bus": "bus-main",
     "menu-settings": "settings-main",
     "menu-agency": "agency-main"
+}
+
+function handle_form_add_franchisee(e) {
+    e.preventDefault();
+    console.log('invoked: check_form_add_franchisee');
+
+    $.ajax({
+        url: 'http://192.168.0.134:5000/entity/franchisee/add',
+        type: 'post',
+        data: $(`#add-franchisee-form`).serialize(),
+        success: function (data) {
+            console.log(data);
+            if (data.success) location.reload();
+            else if (data.error) alert(data.error);
+        }
+    });
+}
+
+function do_list_franchisee_table() {
+    console.log('invoked: do_list_franchisee_table');
+    $(`#list-franchisee-table tbody`).empty();
+    $.ajax({
+        url: 'http://192.168.0.134:5000/entity/franchisee',
+        type: 'post',
+        success: function (data) {
+            console.log(data);
+            if (!data.success) return;
+
+            let new_data = '';
+            data.payload.forEach(function (loop_data, i) {
+                new_data += `
+                <tr>
+                    <td>${i+1}</td>
+                    <td>${loop_data.code}</td>
+                    <td>${loop_data.name}</td>
+                    <td>${loop_data.phone}</td>
+                    <td>${loop_data.address}</td>
+                </tr>`;
+            });
+            $(`#list-franchisee-table tbody`).append(new_data);
+        }
+    });
 }

c1013c51d31319cef3f742b93780354bc405e272
its-davis <80759908+its-davis@users.noreply.github.com>
2023-03-21 10:32:57 +0530

    URL Mods

--- script.js
+++ script.js
@@ -1,39 +1,4 @@
-let server_address = '192.168.0.111:5000'
-
-$(document).ready(function () {
-    do_list_franchisee_table();
-    do_list_agency();
-
-    $('#formula').on('change', function (e) {
-        console.log('selected revenue type: ', this.value);
-        $('#selectFranchise').empty();
-
-        $.ajax({
-            type: "POST",
-            url: 'http://' + server_address + '/ajax_sample/'+this.value,
-            success: function (response_data) {
-                console.log('response_data: ', response_data);
-                let ops = '<option selected disabled>-- CLick to select --</option>'
-                response_data.forEach(function (a, b) {
-                    ops += `<option value="${a}">${a}</option>`
-                });
-                $('#selectFranchise').append(ops);
-            },
-            complete: () => console.log('complete')
-        });
-    });
-
-    $(`body > .container > .navigation > ul li:not(:first-child)`).on('click', function() {
-        $(`body > div:not(:first-child)`).hide()
-        let link_class = this.classList[0];
-        let div_class = link_main_class_names[link_class];
-        $(`body > div.`+div_class).show()
-        console.log(link_class, div_class);
-    });
-
-    $(`#add-franchisee-form`).on('submit', handle_form_add_franchisee);
-    $(`#add-agency-form`).on('submit', handle_form_add_agency);
-});
+let server_address = 'localhost:5000';
 
 let link_main_class_names = {
     "menu-main": "main",
@@ -44,14 +9,33 @@ let link_main_class_names = {
     "menu-add-client-main":"add-client-main",
 }
 
-function handle_form_add_franchisee(e) {
+$(document).ready(function () {
+    $(`body > .container > .navigation > ul li:not(:first-child)`).on('click', function() {
+        $(`body > div:not(:first-child)`).hide()
+        let link_class = this.classList[0];
+        let div_class = link_main_class_names[link_class];
+        $(`body > div.`+div_class).show()
+        console.log(link_class, div_class);
+    });
+
+    $(`#add-franchisee-form`).on('submit', handle_ajax_form);
+    $(`#add-agency-form`).on('submit', handle_ajax_form);
+
+    load_data()
+});
+
+function handle_ajax_form(e) {
     e.preventDefault();
-    console.log('invoked: check_form_add_franchisee');
+    console.log(e);
+    console.log('invoked: ajax form for: ', e.currentTarget.id);
+
+    if (e.currentTarget.action.split('/api').length !== 2) return;
+
 
     $.ajax({
-        url: 'http://' + server_address + '/entity/franchisee/add',
+        url: 'http://' + server_address + '/api' + e.currentTarget.action.split('/api')[1],
         type: 'post',
-        data: $(`#add-franchisee-form`).serialize(),
+        data: $(e.currentTarget).serialize(),
         success: function (data) {
             console.log(data);
             if (data.success) location.reload();
@@ -60,19 +44,24 @@ function handle_form_add_franchisee(e) {
     });
 }
 
-function do_list_franchisee_table() {
-    console.log('invoked: do_list_franchisee_table');
-    $(`#list-franchisee-table tbody`).empty();
+function load_data() {
     $.ajax({
-        url: 'http://' + server_address + '/entity/franchisee',
+        url: 'http://' + server_address + '/api/view/all',
         type: 'post',
         success: function (data) {
-            console.log(data);
-            if (!data.success) return;
+            console.log('view all data: ', data);
+            if (data.success) {
+                if (data.payload.franchisee) load_franchisee_table(data.payload.franchisee);
+                if (data.payload.agency) load_agency_table(data.payload.agency);
+            }
+        }
+    });
+}
 
-            let new_data = '';
-            data.payload.forEach(function (loop_data, i) {
-                new_data += `
+function load_franchisee_table(data) {
+    let new_data = '';
+    data.forEach(function (loop_data, i) {
+        new_data += `
                 <tr>
                     <td>${i+1}</td>
                     <td>${loop_data.code}</td>
@@ -80,47 +69,18 @@ function do_list_franchisee_table() {
                     <td>${loop_data.phone}</td>
                     <td>${loop_data.address}</td>
                 </tr>`;
-            });
-            $(`#list-franchisee-table tbody`).append(new_data);
-        }
     });
+    $(`#list-franchisee-table tbody`).empty().append(new_data);
 }
 
-function handle_form_add_agency(e) {
-    e.preventDefault();
-    console.log('invoked: handle_form_add_agency');
-
-    $.ajax({
-        url: 'http://' + server_address + '/entity/agency/add',
-        type: 'post',
-        data: $(`#add-agency-form`).serialize(),
-        success: function (data) {
-            console.log(data);
-            if (data.success) location.reload();
-            else if (data.error) alert(data.error);
-        }
+function load_agency_table(data) {
+    let new_data = '';
+    data.forEach(function (loop_data, i) {
+        new_data += `
+        <tr>
+            <td>${i+1}</td>
+            <td>${loop_data.name}</td>
+        </tr>`;
     });
-}
-
-function do_list_agency() {
-    console.log('invoked: do_list_franchisee_table');
-    $(`#list-agency-table tbody`).empty();
-    $.ajax({
-        url: 'http://' + server_address + '/entity/agency',
-        type: 'post',
-        success: function (data) {
-            console.log(data);
-            if (!data.success) return;
-
-            let new_data = '';
-            data.payload.forEach(function (loop_data, i) {
-                new_data += `
-                <tr>
-                    <td>${i+1}</td>
-                    <td>${loop_data.name}</td>
-                </tr>`;
-            });
-            $(`#list-agency-table tbody`).append(new_data);
-        }
-    });
+    $(`#list-agency-table tbody`).empty().append(new_data);
 }

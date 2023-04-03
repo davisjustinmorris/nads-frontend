ee9fcc32862663591db288bf0616a7a32b8ab7bc
its-davis <80759908+its-davis@users.noreply.github.com>
2023-04-03 11:59:59 +0530

    load server charges

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let server_address = '192.168.0.111:5000';
+let server_address = 'localhost:5000';
 
 let link_main_class_names = {
     "menu-main": "main",
@@ -64,6 +64,7 @@ function load_data() {
                 if (server_data.profit_ratio) load_profit_ratio_details(server_data.profit_ratio);
                 if (server_data.orders) load_orders(server_data.orders);
                 if (server_data.company) load_company(server_data.company);
+                if (server_data.server) load_server_charges(server_data.server);
 
                 if (server_data.franchisee) {
                     server_data.franchisee.forEach(function (loop_data) {
@@ -369,10 +370,23 @@ function load_company(data) {
             <td>${loop_data['amount']}</td>
             <td>${loop_data['date-created']}</td>
             <td>${loop_data['date-updated']}</td>
-        </tr>
-        `;
+        </tr>`;
     })
     $(`.company-revenue-table tbody`).empty().append(html_data);
+}
+
+function load_server_charges(data) {
+    let html_data = '';
+    data.forEach(function (loop_data, i) {
+        html_data += `
+        <tr>
+            <td>${i}</td>
+            <td>${loop_data['date']}</td>
+            <td>${loop_data['amount']}</td>
+            <td>${loop_data['ad-name']}</td>
+        </tr>`;
+    })
+    $(`.payments-serverCharge-container tbody`).empty().append(html_data);
 }
 
 function on_click__orders_record_payment(context) {
